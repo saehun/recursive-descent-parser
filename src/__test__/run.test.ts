e6ac6796dbf16992f6703b00a3ab3595ca452021
minidonut <nycom13@gmail.com>
2020-12-20 19:14:49 +0900

    Add test for first parser

---
+++ src/__test__/run.test.ts
@@ -0,0 +1,14 @@
+import { Parser } from '../Parser';
+
+const parser = new Parser();
+
+describe('Parser', () => {
+  it('can parse number', () => {
+    const source = '42';
+    const ast = parser.parse(source);
+    expect(ast).toEqual({
+      type: 'NumericLiteral',
+      value: 42,
+    });
+  });
+});
