a3843a331f977179a9f8d9e7495d8ea8be92aa3b
minidonut <nycom13@gmail.com>
2021-01-01 04:41:06 +0900

    Update test util for ease

--- src/__test__/common.ts
+++ src/__test__/common.ts
@@ -1,7 +1,12 @@
 import { Parser } from '../Parser';
 const parser = new Parser();
-export const parseTest = (name: string, source: string, expected: any): void => {
-  return test(name, () => expect(parser.parse(source)).toEqual(expected));
+export const parseTest = (name: string, source: string, ...expected: any[]): void => {
+  return test(name, () =>
+    expect(parser.parse(source)).toEqual({
+      type: 'Program',
+      body: expected,
+    })
+  );
 };
 
 export const parseThrow = (name: string, source: string, expected: any): void => {
