333d9cbd9132af4718df70b3081facac16e4bd0d
minidonut <nycom13@gmail.com>
2021-01-01 07:28:19 +0900

    Add more test case to unary expression

--- src/__test__/unary.test.ts
+++ src/__test__/unary.test.ts
@@ -23,3 +23,23 @@ parseTest('!x', '!x;', {
     },
   },
 });
+
+parseTest('unary has higher presedence than multiplicative', '3 * -x;', {
+  type: 'ExpressionStatement',
+  expression: {
+    type: 'BinaryExpression',
+    operator: '*',
+    left: {
+      type: 'NumericLiteral',
+      value: 3,
+    },
+    right: {
+      type: 'UnaryExpression',
+      operator: '-',
+      argument: {
+        type: 'Identifier',
+        name: 'x',
+      },
+    },
+  },
+});
