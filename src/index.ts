85faf909d36ab2c186cbf4c91a41665a1bed7578
minidonut <nycom13@gmail.com>
2021-01-01 22:52:27 +0900

    Bind with CLI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello recursive-descent-parser");
+import { Parser } from './Parser';
+import * as fs from 'fs';
+
+function main(argv: string[]) {
+  const [, , mode, exp] = argv;
+  const parser = new Parser();
+
+  let ast = null;
+
+  if (mode === '-e') {
+    ast = parser.parse(exp);
+  }
+
+  if (mode === '-f') {
+    const src = fs.readFileSync(exp, 'utf-8');
+    ast = parser.parse(src);
+  }
+
+  console.log(JSON.stringify(ast, null, 2));
+}
+
+main(process.argv);
